8389fb01ac09c7c9608d293a8eb6ed55f2af0505
student <student@debian>
2013-12-11 17:19:39 +0000

    After db and 3 tables created

---
+++ test/unit/uk/ac/shu/webarch/eregister/InstructorSpec.groovy
@@ -0,0 +1,20 @@
+package uk.ac.shu.webarch.eregister
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Instructor)
+class InstructorSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
---
+++ test/unit/uk/ac/shu/webarch/eregister/RegClassSpec.groovy
@@ -0,0 +1,20 @@
+package uk.ac.shu.webarch.eregister
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(RegClass)
+class RegClassSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
